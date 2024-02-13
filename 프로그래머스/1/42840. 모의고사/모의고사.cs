6f9a9d7fe6800bab78a210827701a9b47dfc68c9
Jang Yeongjun <150320634+Jang-Yeongjun@users.noreply.github.com>
2024-02-13 09:12:06 +0900

    [level 1] Title: 모의고사, Time: 1.35 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/1/42840. 모의고사/모의고사.cs
@@ -2,40 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Solution 
-{
-    public int[] solution(int[] answers) 
-    {
-        int[][] patterns = new int[][] 
+public class Solution {
+    public int[] solution(int[] answers) {
+        int[] nRules1 = new int[] { 1, 2, 3, 4, 5 };
+        int[] nRules2 = new int[] { 2, 1, 2, 3, 2, 4, 2, 5 };
+        int[] nRules3 = new int[] { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+        int[] nScores = new int[3];
+        for (int i = 0; i < answers.Length; i++)
         {
-            new int[] {1, 2, 3, 4, 5}, // 1번 수포자의 패턴
-            new int[] {2, 1, 2, 3, 2, 4, 2, 5}, // 2번 수포자의 패턴
-            new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5, 5} // 3번 수포자의 패턴
-        };
-
-        int[] score = new int[3]; // 각 수포자의 점수를 저장할 배열
-
-        for (int i = 0; i < answers.Length; i++) 
-        {
-            for (int j = 0; j < patterns.Length; j++) 
-            {
-                if (answers[i] == patterns[j][i % patterns[j].Length]) 
-                {
-                    score[j]++;
-                }
-            }
+            if (answers[i] == nRules1[i % nRules1.Length]) ++nScores[0];
+            if (answers[i] == nRules2[i % nRules2.Length]) ++nScores[1];
+            if (answers[i] == nRules3[i % nRules3.Length]) ++nScores[2];
         }
-
-        int maxScore = score.Max(); // 가장 높은 점수 찾기
-        List<int> winners = new List<int>();
-        for (int i = 0; i < score.Length; i++) 
-        {
-            if (score[i] == maxScore) 
-            {
-                winners.Add(i + 1); // 인덱스가 0부터 시작하므로 1을 더해줌
-            }
-        }
-
-        return winners.OrderBy(x => x).ToArray(); // 오름차순 정렬 후 반환
+        int maxScore = nScores.Max(); // 최대 점수 계산
+        List<int> lstAnswer = new List<int>();
+        if (nScores[0] == maxScore) lstAnswer.Add(1);
+        if (nScores[1] == maxScore) lstAnswer.Add(2);
+        if (nScores[2] == maxScore) lstAnswer.Add(3);
+        return lstAnswer.ToArray();
     }
 }
